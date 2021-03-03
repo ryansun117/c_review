2dce872c02029a86a9dab166ca6279a81a3284b8
Xuxiang Sun <xuxiangs@royal-16.cs.wisc.edu>
2021-03-03 09:33:26 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include "./linked_list.h"
+
+int main(int argv, char** argc) {
+    
+    printf("Hello world!\n");
+    
+    node* head = NULL;
+    int i = 0;
+    head = add_node(head, 11);
+    print_list(head);
+    printf("\n");
+
+    for(i=0; i< 10; i++){
+        head = add_node(head, i);
+        print_list(head);
+        printf("\n");
+    }
+
+    for(i=10; i > 0; i--){
+        head = remove_node(head, i);
+        print_list(head);
+        printf("\n");
+    }
+
+    head = remove_node(head, 11);
+    print_list(head);
+    head = remove_node(head, 0);
+    print_list(head);
+    return 0;
+}
